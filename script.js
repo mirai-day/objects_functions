4d5c629b6a1522531be0531f955adb23d8878ace
miraida <mirai001508@gmail.com>
2022-05-17 19:28:06 +0600

    text

---
+++ script.js
@@ -0,0 +1,42 @@
+//Object
+
+// let obj = {
+//   name: "Miraida",  
+//   age: 20,
+//   hobbies: {
+//       bir: "dance",
+//       eki: "sing"
+//   }
+// }
+// //  console.log(Object.keys(obj))
+// console.log(Object.values(obj))
+
+//Функция
+// let num = [ 10, 20, 30, 40] 
+// function mir(arr){
+//     document.write(arr)
+//     console.log(arr)
+// }
+// mir(num)
+
+// let mir= document.getElementById('btn')
+
+// mir.addEventListener('click', aid)
+// function aid(){
+//     alert('мени бастын!')
+// }
+
+let one= document.querySelector('.plus')
+one.addEventListener('mouseout', func)
+let cm = document.getElementById('cm')
+function func(){
+    cm.style.fontSize ='50px'
+}
+
+
+
+ 
+
+
+    
+
